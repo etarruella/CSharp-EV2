e547e8c4495ba9e687de545559f1cf012104120d
Erik T. Segura <eriktarruella@gmail.com>
2025-06-13 06:37:24 +0000

    feat: improving store & storeAt. formatting doc

--- PlayerInventory.cs
+++ PlayerInventory.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 
-namespace VideoGame.Inventory {
+namespace VideoGame.Inventory
+{
 
-    public partial class PlayerInventory {
+    public partial class PlayerInventory
+    {
 
         /// <summary>
         /// Jugador que contiene al inventario
@@ -25,44 +27,59 @@ namespace VideoGame.Inventory {
         /// </summary>
         /// <param name="player">Dueño del inventario</param>
         /// <param name="size">Capacidad máxima del inventario</param>
-        public PlayerInventory(Player player, int size = 10) {
+        public PlayerInventory(Player player, int size = 10)
+        {
             parent = player;
             Size = size;
             items = new IItem?[size];
         }
 
-        /// <summary>
-        /// Almacena un ítem en la primera posición libre
-        /// </summary>
-        public bool Store(IItem item) {
-            for (int i = 0; i < items.Length; i++) {
-                if (items[i] == null) {
+        public bool Store(IItem item)
+        {
+            if (Contains(item))
+            {
+                return true; // Ya está en el inventario, no lo volvemos a insertar
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
                     items[i] = item;
-                    if (item is Item concreteItem) {
+                    if (item is Item concreteItem)
+                    {
                         concreteItem.MoveTo(this);
                     }
                     return true;
                 }
             }
-            return false;
+
+            return false; // Inventario lleno
         }
 
-        /// <summary>
-        /// Almacena un ítem en una posición específica si está vacía
-        /// </summary>
-        public bool StoreAt(IItem item, int index) {
+        public bool StoreAt(IItem item, int index)
+        {
             if (index < 0 || index >= Size || items[index] != null) return false;
+
+            if (Contains(item))
+            {
+                return true; // Ya está en el inventario, no lo insertamos otra vez
+            }
+
             items[index] = item;
-            if (item is Item concreteItem) {
+            if (item is Item concreteItem)
+            {
                 concreteItem.MoveTo(this);
             }
             return true;
         }
 
+
         /// <summary>
         /// Obtiene el ítem en una posición o null si no hay
         /// </summary>
-        public IItem? GetItemAt(int index) {
+        public IItem? GetItemAt(int index)
+        {
             if (index < 0 || index >= Size) return null;
             return items[index];
         }
@@ -70,11 +87,15 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Elimina un ítem del inventario si se encuentra
         /// </summary>
-        public bool Drop(IItem item) {
-            for (int i = 0; i < items.Length; i++) {
-                if (items[i] == item) {
+        public bool Drop(IItem item)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == item)
+                {
                     items[i] = null;
-                    if (item is Item concreteItem) {
+                    if (item is Item concreteItem)
+                    {
                         concreteItem.MoveTo(null!);
                     }
                     return true;
@@ -86,9 +107,11 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Elimina un ítem de una posición específica
         /// </summary>
-        public bool Drop(int index) {
+        public bool Drop(int index)
+        {
             if (index < 0 || index >= Size || items[index] == null) return false;
-            if (items[index] is Item concreteItem) {
+            if (items[index] is Item concreteItem)
+            {
                 concreteItem.MoveTo(null!);
             }
             items[index] = null;
@@ -98,9 +121,11 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Devuelve una colección con todos los ítems actuales
         /// </summary>
-        public ICollection<IItem> ListItems() {
+        public ICollection<IItem> ListItems()
+        {
             List<IItem> currentItems = new List<IItem>();
-            foreach (var item in items) {
+            foreach (var item in items)
+            {
                 if (item != null) currentItems.Add(item);
             }
             return currentItems;
@@ -109,8 +134,10 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Indica si un ítem está en el inventario
         /// </summary>
-        public bool Contains(IItem item) {
-            foreach (var i in items) {
+        public bool Contains(IItem item)
+        {
+            foreach (var i in items)
+            {
                 if (i == item) return true;
             }
             return false;
@@ -119,8 +146,10 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Busca un ítem que cumpla una condición
         /// </summary>
-        public IItem? Find(Func<IItem, bool> condition) {
-            foreach (var item in items) {
+        public IItem? Find(Func<IItem, bool> condition)
+        {
+            foreach (var item in items)
+            {
                 if (item != null && condition(item)) return item;
             }
             return null;
@@ -129,8 +158,10 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Busca un ítem de tipo T que cumpla una condición
         /// </summary>
-        public T? Find<T>(Func<T, bool> condition) where T : class, IItem {
-            foreach (var item in items) {
+        public T? Find<T>(Func<T, bool> condition) where T : class, IItem
+        {
+            foreach (var item in items)
+            {
                 if (item is T typedItem && condition(typedItem)) return typedItem;
             }
             return null;
@@ -139,9 +170,12 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Vacía completamente el inventario
         /// </summary>
-        public void Clear() {
-            for (int i = 0; i < items.Length; i++) {
-                if (items[i] is Item concreteItem) {
+        public void Clear()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] is Item concreteItem)
+                {
                     concreteItem.MoveTo(null!);
                 }
                 items[i] = null;
@@ -151,7 +185,8 @@ namespace VideoGame.Inventory {
         /// <summary>
         /// Transfiere un ítem a otro inventario
         /// </summary>
-        public bool Transfer(IItem item, PlayerInventory target) {
+        public bool Transfer(IItem item, PlayerInventory target)
+        {
             if (!Contains(item)) return false;
             if (!target.Store(item)) return false;
             return Drop(item);
